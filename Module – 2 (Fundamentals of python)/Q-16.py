3c27326ee63e00887a4d9ecee8e27deb4603b02f
PIYUSHBALWANI22 <135206000+PIYUSHBALWANI22@users.noreply.github.com>
2024-10-31 19:18:46 +0530

    Update Q-16.py

--- Module – 2 (Fundamentals of python)/Q-16.py
+++ Module – 2 (Fundamentals of python)/Q-16.py
@@ -1,2 +1,14 @@
 """Write a Python program to count the occurrences of each word in a given sentence """
 
+sentence = "Here is a small world in a world which is in another world"
+words = sentence.split()   
+wordCount = {}   
+
+for word in words:
+    if word in wordCount:
+        wordCount[word]+=1   
+    else:
+        wordCount[word]=1   
+
+for word, count in wordCount.items():
+    print(f"'{word}' occurs {count} time(s).")
