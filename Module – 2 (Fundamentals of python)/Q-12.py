771feed80287da663bf8f07176dddfd087542162
PIYUSHBALWANI22 <135206000+PIYUSHBALWANI22@users.noreply.github.com>
2026-02-16 11:49:35 +0530

    Delete Module – 2 (Fundamentals of python) directory

--- Module – 2 (Fundamentals of python)/Q-12.py
+++
@@ -1,4 +0,0 @@
-"""Write a Python program to calculate the length of a string."""
-
-str= (input("Enter a string : "))
-print(f"The lenth of string is",len(str))
--- Module – 2 (Fundamentals of python)/Q-2.py
+++
@@ -1,8 +0,0 @@
-"""Write a Python program to get the Factorial number of given number."""
-
-frl = 1
-
-num = int(input("Enter a number: "))
-for i in range (1,num+1):
-    frl *=i
-print(frl)
